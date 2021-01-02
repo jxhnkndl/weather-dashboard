47ab17563ac49ed75460c202e98e5be078db96e7
jxhnkndl <jkroyston@gmail.com>
2021-01-01 21:47:35 -0500

    Create AJAX request and test for valid API response

--- app.js
+++ app.js
@@ -13,5 +13,31 @@ $(document).ready(function() {
     $("#today").text(currentDate);
   }
 
+  // Get weather from API
+  function getWeather() {
+    var city = $("#search").val();
+    var units = "imperial";
+    var APIKey = "f4d6848eb3a488816cecbd2392d8a108";
+    var queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${APIKey}`;
+
+    if (city === "") {
+      console.log("Invalid City");
+      return;
+    }
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+    });
+
+  }
+
+  // Event Listener: Search Button
+  $("#search-form").on("submit", function(event) {
+    event.preventDefault();
+    getWeather();
+  });
 
 });

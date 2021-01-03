164d57d885aa9ab6c5dd0f1ae76e4c3c178d7933
jxhnkndl <jkroyston@gmail.com>
2021-01-02 21:16:01 -0500

    Switch base url protocal to https

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
 
   // API Query Parameters
-  var baseURL = "http://api.openweathermap.org/data/2.5/";
+  var baseURL = "https://api.openweathermap.org/data/2.5/";
   var APIKey = "f4d6848eb3a488816cecbd2392d8a108";
   var units = "imperial";
 

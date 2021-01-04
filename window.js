7dac51a592dcf56660cab85dea50a8d479f84cb1
jxhnkndl <jkroyston@gmail.com>
2021-01-03 19:53:35 -0500

    Fix search history display bug

--- window.js
+++ window.js
@@ -1,5 +1,13 @@
 // Expand collapsed search history on displays > 578px
 $(document).ready(function() {
+
+  // Set initial visibility conditions
+  $("#search-history").addClass("show");
+  $("#collapse-search-history").hide();
+
+  
+
+  // Show or hide search history based on browser window size
   $(window).resize(function() {
     var w = $(window).width();
 
